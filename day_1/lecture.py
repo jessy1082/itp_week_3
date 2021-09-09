1e61b3b4080fbbaf00bc455a03701c5187cf37fe
J C <jessy1082@yahoo.com>
2021-09-09 19:23:40 -0400

    Review

--- day_1/lecture.py
+++ day_1/lecture.py
@@ -1,6 +1,10 @@
 # ITP Week 3 Day 1 Lecture
 
 #------------PIP-----------
+# C - create
+# R - read
+# U - update
+# D - delete
 
 #------------EXCEL SPREADSHEET-----------
 
@@ -20,81 +24,93 @@
 
 # -openpyxl is a downloaded module
 # -Download & install openpyxl using pip
-#   pip install openpyxl
+#   pip3 install openpyxl (on gitbash)
 
 import openpyxl
 
 #Assign your .xlsx file to a variable
 filename = "C:/Users/jessy/Desktop/Vetstech_Python/itp_week_3/day_1/lecture.xlsx"
 wb = openpyxl.load_workbook(filename) #returns a Workbook object.
-
-#my_workbook = openpyxl.load_workbook('example.xlsx')
-#use the 'type' method on the new variable to verify what kind of data type you are working with
-print(type(wb)) #<class 'openpyxl.workbook.workbook.Workbook'>
-
-sheet_name = wb.sheetnames
-print(sheet_name) #['Sheet1', 'Population by Census Tract']
-
-#Each Excel file can have multiple sheets, which each contain many rows, columns and cells.  You must specify which sheet you would like to manipulate.
-#get_sheet_names() #and
-#get_sheet_by_name() #help get Worksheet objects.
-
-#my_workbook.get_sheet_names()  # Result-->  ['Sheet1', 'Sheet2', 'Sheet3']
-
-my_sheet = wb['Sheet1']
-print(my_sheet) # <Worksheet "Sheet1">
-# wb.get_sheet_by_name('sheet_name[0]')
+print(type(wb)) #<class 'openpyxl.workbook.workbook.Workbook'>  
+# #my_workbook = openpyxl.load_workbook('example.xlsx')
+# #use the 'type' method on the new variable to verify what kind of data type you are working with
+# print(type(wb)) #<class 'openpyxl.workbook.workbook.Workbook'>
+#type(my_workboo)
 
 
-# my_sheet = my_workbook.get_sheet_by_name('Sheet 1')
-# verify
-# type(my_sheet)  # Result-->  <class 'openpyxl.worksheet.worksheet.Worksheet'>
+sheet_name = wb.sheetnames #property of specific objest
+print(sheet_name)
 
-apple_cell = my_sheet['B1'] 
-print(apple_cell.value) # Apples
+
+# sheet_name = wb.sheetnames
+# print(sheet_name) #['Sheet1', 'Population by Census Tract']
+
+# #Each Excel file can have multiple sheets, which each contain many rows, columns and cells.  You must specify which sheet you would like to manipulate.
+# #get_sheet_names() #and
+# #get_sheet_by_name() #help get Worksheet objects.
+
+# #my_workbook.get_sheet_names()  # Result-->  ['Sheet1', 'Sheet2', 'Sheet3']
+
+my_sheet = wb['Sheet1'] #key is the sheetnames itself
+
+#my_sheet = wb.get_sheet_by_name('Sheet1')
+#wb.get_sheet_by_name(sheet_name[0])  to get sheet name
+
+# my_sheet = wb['Sheet1']
+# print(my_sheet) # R: <Worksheet "Sheet1">
+
+
+# # my_sheet = my_workbook.get_sheet_by_name('Sheet 1')
+# # verify
+# # type(my_sheet)  # Result--> R: <class 'openpyxl.worksheet.worksheet.Worksheet'>
+
+apple_cell = my_sheet['B1'] #object
+print(apple_cell.value) # R: Apples
 
 another_cell = my_sheet['B3'] 
-print(another_cell.value) # Pears
+print(another_cell.value) # R: Pears
 
 print('Row %s, column %s, is %s' %(another_cell.row, another_cell.column, another_cell.value))
 
-print('COORDINATE %s is %s' % (another_cell.coordinate, another_cell.value))
+print('COORDINATE %s is %s' %(another_cell.coordinate, another_cell.value))
 
-# Cell objects have a "value" member variable with the content of that cell.
-# The square brackets in sheet['A1'] get Cell objects.
-# The cell() method also returns a Cell object from a sheet.
-#my_sheet['A1'] #will return the CELL OBJECT (NOT the value you see in the cell) created by the intersection of the first column and the first row.  The reult of sheet['A1'] will actually be:
-    # Result -->  <Cell Sheet1.A1>
-#so we must ASSIGN IT TO A VARIABLE before we can work with it.
-#my_cell = sheet['A1']
-#my_cell.value() # Result --> the value of the A1 cell
+# # Cell objects have a "value" member variable with the content of that cell.
+# # The square brackets in sheet['A1'] get Cell objects.
+# # The cell() method also returns a Cell object from a sheet.
+# #my_sheet['A1'] #will return the CELL OBJECT (NOT the value you see in the cell) created by the intersection of the first column and the first row.  The reult of sheet['A1'] will actually be:
+#     # Result -->  <Cell Sheet1.A1>
+# #so we must ASSIGN IT TO A VARIABLE before we can work with it.
+# #my_cell = sheet['A1']
+# #my_cell.value() # Result --> the value of the A1 cell
 
-#Don't forget to convert the values to a string if you are working with strings
-#str(my_cell.value)
-#str(my_sheet['A1'].value)
+# #Don't forget to convert the values to a string if you are working with strings
+# #str(my_cell.value)
+# #str(my_sheet['A1'].value)
 
-#You can also access cell values using the .cell() method and passing arguments for the column and row:
-#my_sheet.cell(row=1, column=2) # Result --> <Cell Sheet1.B1>
-
-
-#Python can be used to loop through the cells on your spreadsheet and manipulate data automatically.
-#for i in range(1, 8):
- #   print(i, my_sheet.cell(row = i, column =2).value)
-#The code may be difficult to understand at first, but notice that in 'row = i', the 'i' will increase by 1 until it reaches 8, thus printing out each value in column 2.  The first 'i', after the 'print(' is to show the correlation between 'i' and row being printed.
+# #You can also access cell values using the .cell() method and passing arguments for the column and row:
+# #my_sheet.cell(row=1, column=2) # Result --> <Cell Sheet1.B1>
 
 
-# READ DATA
 
-# WRITE TO A DATA
 
-# UPDATE A SPREADSHEET
+# #Python can be used to loop through the cells on your spreadsheet and manipulate data automatically.
+# #for i in range(1, 8):
+#  #   print(i, my_sheet.cell(row = i, column =2).value)
+# #The code may be difficult to understand at first, but notice that in 'row = i', the 'i' will increase by 1 until it reaches 8, thus printing out each value in column 2.  The first 'i', after the 'print(' is to show the correlation between 'i' and row being printed.
 
-# SET UP A DATA STRUCTURE WITH THE UPDATED INFORMATION
 
-# FORMULAS
+# # READ DATA
 
-# OTHER FUNCTIONALITIES
+# # WRITE TO A DATA
 
-# SUMMARY
+# # UPDATE A SPREADSHEET
+
+# # SET UP A DATA STRUCTURE WITH THE UPDATED INFORMATION
+
+# # FORMULAS
+
+# # OTHER FUNCTIONALITIES
+
+# # SUMMARY
 
 
--- day_1/practice.py
+++ day_1/practice.py
@@ -4,9 +4,11 @@ wb = openpyxl.load_workbook("C:/Users/jessy/Desktop/Vetstech_Python/itp_week_3/d
 print(type(wb))
 
 sheet= wb['Sheet1']
+print(sheet.max_row +1) # R: 7
 
+# for i in range(1, sheet.max_row +1):
 for i in range(1,8):
-    # on the date of A, C amount of B
+    # on the date of A, C amount of Bwere sold
     date= "A" + str(i)
     date_cell = sheet[date]
 
@@ -16,5 +18,5 @@ for i in range(1,8):
     fruit = "B" + str(i)
     fruit_cell = sheet[fruit]
 
-    print ("On the date of" + str(date_cell.value) + "," + 
+    print ("On the date of " + str(date_cell.value) + "," + 
     str(amount_cell.value) + " amount of " + fruit_cell.value + "!")
